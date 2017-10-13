78eb1caefe6b407f46bacbfc7047d2a4e32e84f2
Adeline Harcourt <adelineharcourt@gmail.com>
2017-10-13 16:08:44 -0400

    Updated crawler.js and added searches.js

--- crawler.js
+++ crawler.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var request = require('request');
+var cheerio = require('cheerio');
 var bodyParser = require('body-parser');
-var mysql = require('./dbcon');
+var mysql = require('./lib/dbcon');
+var search = require('./lib/searches');
 
 // Create server object
 var app = express();
@@ -13,13 +15,22 @@ app.use(express.static('public'));
 
 // Route to main page
 app.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+    res.sendFile('index.html');
 });
 
 // POST to crawler
 app.post('/crawl', function(req, res, next) {
-  console.log(req.body);
-  res.send('POST successful.');
+    var edges = [];
+    
+    if(req.body.SearchType == "BFS") {
+        search.breadthFS(req.body.RootURL, req.body.SearchDepth, edges, function(err){
+            if(err){ 
+                next(err); 
+                return; 
+            }
+            res.send(edges);
+        });
+    }
 });
 
 // 404 Error
