0bf49ae2c65af4caa254c3ba83a7a13a7387e2b2
Dalrymple <ian.dalrymple@stryker.com>
2017-10-24 07:35:27 -0400

    All five wrappers completed

--- SQL/TestingScratchpad.sql
+++ SQL/TestingScratchpad.sql
@@ -3,8 +3,8 @@ SELECT * FROM wc_TempEdges;
 SELECT * FROM wc_Users;
 SELECT * FROM wc_Urls;
 SELECT * FROM wc_Edges;
-SELECT * FROM wc_UserSearches;
-SELECT * FROM wc_SingleGraphs;
+SELECT * FROM wc_UserSearches WHERE Id = 92;
+SELECT * FROM wc_SingleGraphs WHERE UserSearchId = 92;
 
 -- Order of operations 
 SET @resLocal = -99;
@@ -22,3 +22,14 @@ SELECT @resLocal; SET @resLocal = -99;
 
 CALL wcSp_InsertNewTree("http://newCookie", "RogerPedacker", 11, "BFS", 22, @resLocal);
 SELECT @resLocal; SET @resLocal = -99;
+
+DELETE FROM wc_TempEdges;
+SET @url1 = CONCAT('http://', CONCAT(CAST(unix_timestamp() AS char), '1'));
+SET @url2 = CONCAT('http://', CONCAT(CAST(unix_timestamp() AS char), '2'));
+SET @url3 = CONCAT('http://', CONCAT(CAST(unix_timestamp() AS char), '3'));
+SET @url4 = CONCAT('http://', CONCAT(CAST(unix_timestamp() AS char), '4'));
+SET @maxCookie = (SELECT MAX(CookieId) FROM wc_UserSearches);
+INSERT INTO wc_TempEdges(SorcUrl, DestUrl, CookieId)   
+VALUES(@url1, @url2, (@maxCookie + 1)), (@url3, @url4, (@maxCookie + 1));
+
+SELECT * FROM wc_TempEdges;
